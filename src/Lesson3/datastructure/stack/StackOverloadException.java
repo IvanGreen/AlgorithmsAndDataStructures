5a14db95247d3899502d0422a5d7518be9cebb84
IvanGreen <mylinkou@bk.ru>
2019-05-24 16:26:10 +0300

    Added Lesson 3 HomeWork

--- src/Lesson3/datastructure/stack/StackOverloadException.java
+++
@@ -1,8 +0,0 @@
-package Lesson3.datastructure.stack;
-
-public class StackOverloadException extends RuntimeException {
-
-    public StackOverloadException() {
-        super("ru.geekbrains.datastructure.stack.Stack is full");
-    }
-}
