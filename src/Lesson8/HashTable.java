6755b7e86009c25c778dabbf647f1941d23b8de7
IvanGreen <mylinkou@bk.ru>
2019-06-19 11:28:09 +0300

    - Added Lesson 6 & HomeWork Lesson 6; - Added Lesson 7 & HomeWork Lesson 7; - Added Lesson 8 & HomeWork Lesson 8;

---
+++ src/Lesson8/HashTable.java
@@ -0,0 +1,16 @@
+package Lesson8;
+
+public interface HashTable {
+
+    boolean put(Item item, Integer cost);
+
+    Integer get(Item item);
+
+    boolean remove(Item item);
+
+    int getSize();
+
+    boolean isEmpty();
+
+    void display();
+}
